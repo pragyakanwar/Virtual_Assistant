6dc1add0fc517bf05611ac3c36250786bf24fc5e
pragyakanwar <shekhawatpragya2@gmail.com>
2026-05-15 10:39:12 +0530

    Update music_library.py

--- music_library.py
+++ music_library.py
@@ -1,8 +1,17 @@
 music = {
- "Ranjheye ve" : "https://www.youtube.com/watch?v=55c6IlV7BEo",
+ "ranjheye ve" : "https://www.youtube.com/watch?v=55c6IlV7BEo",
 
-"Kooch na karin" : "https://www.youtube.com/watch?v=7ODrsDrl3qk",
+"kooch na karin" : "https://www.youtube.com/watch?v=7ODrsDrl3qk",
 
-"weapon" : "https://www.youtube.com/watch?v=5VubhvUQaD8"
+"weapon" : "https://www.youtube.com/watch?v=5VubhvUQaD8",
 
+"not guilty" : "https://youtu.be/E7ergOnpO1Q?si=S-MTeHUtkLIFqcCK",
+
+"roots" :"https://youtu.be/e36s_nZCD94?si=M4Ml8W7YG0KZ2Hma",
+
+"bairan" :"https://youtu.be/wb1aIWeftuQ?si=yTvGAOTkK9wG4ES2",
+
+"baby" :"https://youtu.be/kffacxfA7G4?si=OcxRVlXEg3ixJ6Cx",
+
+"shakira":"https://youtu.be/pRpeEdMmmQ0?si=bJyR5Jrr0lmbAT6k"
 }
